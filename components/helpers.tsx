ac1538762e175f1ec07575b1791dd1ceef635d70
Juwan Wheatley <datwheat@gmail.com>
2019-06-24 16:04:02 -0700

    use array syntax for style prop

--- components/helpers.tsx
+++ components/helpers.tsx
@@ -3,7 +3,7 @@ import { Children, cloneElement } from "react";
 export const FirstChildGetsNoSpecialTreatment = ({ children, injectedStyle }) =>
   Children.map(children, (child, i) => {
     if (i !== 0) {
-      const style = { ...(child.props.style || {}), ...injectedStyle };
+      const style = [child.props.style || {}, injectedStyle];
 
       return cloneElement(child, { style });
     }
